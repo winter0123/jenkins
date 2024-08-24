01a71cd0fa6725c17b17c168c73437b1605f54b1
winter0123 <cloudmist.j@gmail.com>
2024-08-24 19:29:32 +0900

    버전 변경

--- src/main/java/com/example/jenkins/controller/TestController.java
+++ src/main/java/com/example/jenkins/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @GetMapping(value = "/")
     public ResponseEntity<String> test() {
         String name = testService.testService();
-        System.out.println("v1.4");
+        System.out.println("v1.5");
         return ResponseEntity.ok(name);
     }
 }
