298e090ad29a5c0182727ce0a9eb4833f1bb20c3
winter0123 <cloudmist.j@gmail.com>
2024-08-24 16:36:40 +0900

    yml 수정

--- src/main/java/com/example/jenkins/controller/TestController.java
+++ src/main/java/com/example/jenkins/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @GetMapping(value = "/")
     public ResponseEntity<String> test() {
         String name = testService.testService();
-        System.out.println("v1.2");
+        System.out.println("v1.3");
         return ResponseEntity.ok(name);
     }
 }
